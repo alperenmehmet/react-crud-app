926139203bef01734930c1b8888fc37a2ec977b7
Mehmet ALPEREN <me.alperen@gmail.com>
2021-11-14 14:23:50 +0300

    Components deleted.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import UserTable from './components/UserTable';
 import data from './components/data';
 import { UserProvider } from './UserContext';
@@ -6,24 +6,7 @@ import AddUserForm from './components/AddUserForm';
 import EditUserForm from './components/EditUserForm';
 
 const App = () => {
-  const [isEditing, setIsEditing] = useState(false);
-  return (
-    <UserProvider>
-      <div className='container'>
-        <h1>React CRUD App</h1>
-        <div className='flex-row'>
-          <div className='flex-large'>
-            <h2>Add user</h2>
-            <AddUserForm />
-          </div>
-          <div className='flex-large'>
-            <h2>View users</h2>
-            <UserTable />
-          </div>
-        </div>
-      </div>
-    </UserProvider>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,46 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from '../UserContext';
-import users from './data';
-
 const AddUserForm = () => {
-  const [user, setUser] = useState({ id: 'null', name: '', username: '' });
-  const [users, setUsers] = useContext(UserContext);
-
-  const addUser = (e) => {
-    e.preventDefault();
-    if (user.name && user.username) {
-      user.id = users.length + 1;
-      setUsers([...users, user]);
-      setUser({ name: '', username: '' });
-    }
-  };
-
-  const onSubmitHandler = (e) => {
-    const { name, value } = e.target;
-    setUser({ ...user, [name]: value });
-  };
-
-  return (
-    <form>
-      <label>Name:</label>
-      <input
-        type='text'
-        name='name'
-        value={user.name}
-        onChange={onSubmitHandler}
-      />
-      <label>User Name:</label>
-      <input
-        type='text'
-        name='username'
-        value={user.username}
-        onChange={onSubmitHandler}
-      />
-      <button type='submit' onClick={addUser}>
-        Add New User
-      </button>
-    </form>
-  );
+  return <form></form>;
 };
 
 export default AddUserForm;
